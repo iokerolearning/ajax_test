6e1462ef1238fc9bdfda062f9af7115593f89b4b
Wenceslao Paez <wcpaez@gmail.com>
2017-09-19 22:45:04 -0500

    Allow to create an alumno

--- js/ajaxify.js
+++ js/ajaxify.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $("#cargando").hide();
+  $(".messages").hide();
 
   var cargarAlumnos = function() {
     $("#cargando").show();
     console.log("Limpiando tabla")
-    $("#total-alumnos tbody").html("")
+    $("#alumnos tbody").html("")
     console.log("Se inicia la carga de alumnos...")
     $.ajax({
       url: 'http://learning1.iokero.com/alumnos.json',
@@ -16,13 +17,36 @@ $(document).ready(function() {
           var row = alumnoRow.replace("_id_", alumno.id).
             replace("_nombres_", alumno.nombres).replace("_apellidos_", alumno.apellidos).
             replace("_codigo_", alumno.codigo)
-          $("#total-alumnos tbody").append(row)
+          $("#alumnos tbody").append(row)
         }
         $("#cargando").hide();
       },
     })
   }
 
-  $("#total-alumnos #cargar-alumnos").on("click", cargarAlumnos)
-  
+  $("#alumnos #cargar-alumnos").on("click", cargarAlumnos)
+  $("#alumnos form").submit(function(event) {
+    $(".messages").hide();
+    event.preventDefault();
+    $.ajax({
+      url: 'http://learning1.iokero.com/alumnos.json',
+      type : "POST",
+      dataType : 'json',
+      data : $("#alumnos form").serialize(),
+      success : function(result) {
+        var row = alumnoRow.replace("_id_", result.id).
+          replace("_nombres_", result.nombres).replace("_apellidos_", result.apellidos).
+          replace("_codigo_", result.codigo)
+        $("#alumnos tbody").append(row)
+      },
+      error: function(xhr) {
+        if(xhr.status == 422){
+          $(".messages .alert").html("")
+          $(".messages .alert").append("<strong>Error</strong>" + xhr.responseText)
+          $(".messages").show()
+        }
+      }
+    })
+
+  });
 });
